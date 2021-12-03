4957e3a4297b3042debe107068b805a125316914
eduardoconceic08 <94643936+eduardoconceic08@users.noreply.github.com>
2021-12-03 13:27:16 -0300

    fixed typo

--- test/TestBurner.js
+++ test/TestBurner.js
@@ -6,7 +6,7 @@ const Purpose = artifacts.require("Purpose");
 const Burner = artifacts.require("Burner");
 
 contract("Burner", function(accounts) {
-  const [owner] = accounts;
+  const [owner, user1] = accounts;
   const supplier = owner;
   const burnPerweiYearly = web3.toWei(0.2, "ether");
   let purpose;
@@ -43,8 +43,42 @@ contract("Burner", function(accounts) {
     await purpose.adminAddRole(burner.address, "burn");
   });
 
+  it("after 1 seconds no burn", async function() {
+    const seconds = 1;
+    const balance = await purpose.balanceOf(supplier);
+
+    await increaseTime(seconds);
+    const burnable = await burner.burnable();
+
+    isSecondsAcurrateEnough(seconds, balance, burnable);
+  });
+
+  it("after 1 seconds, 1 prps left no burn", async function() {
+    const seconds = 1;
+    const balanceToRemove = new web3.BigNumber(1e27).minus(1e18);
+    await purpose.transfer(user1, balanceToRemove);
+    const balance = await purpose.balanceOf(supplier);
+
+    await increaseTime(seconds);
+    const burnable = await burner.burnable();
+
+    isSecondsAcurrateEnough(seconds, balance, burnable);
+  });
+
   it("after 1 minute no burn", async function() {
     const seconds = duration.minutes(1);
+    const balance = await purpose.balanceOf(supplier);
+
+    await increaseTime(seconds);
+    const burnable = await burner.burnable();
+
+    isSecondsAcurrateEnough(seconds, balance, burnable);
+  });
+
+  it("after 1 minute, 1 prps left no burn", async function() {
+    const seconds = duration.minutes(1);
+    const balanceToRemove = new web3.BigNumber(1e27).minus(1e18);
+    await purpose.transfer(user1, balanceToRemove);
     const balance = await purpose.balanceOf(supplier);
 
     await increaseTime(seconds);
